895f1d3c52baa99efb139a44df1ded6f74c90be3
sheb-sheb <sheba8367@gmail.com>
2020-08-20 11:10:21 -0700

    added comments for learning and pictures for icons

--- scripts.js
+++ scripts.js
@@ -1,3 +1,7 @@
+/*
+<body onload = (function)> means that the function will happen when the body 
+    is loaded like those annoying 'remove adblock' popup things
+*/
 function sayHello() {
     alert("Hello World");
 }
